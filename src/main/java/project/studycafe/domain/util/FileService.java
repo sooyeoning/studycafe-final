4367e8586e745fff713d716b16be0f35f4836937
sooyeoning <susan0915@nate.com>
2024-04-01 14:18:57 +0900

    feat 파일 업로드 수정

--- src/main/java/project/studycafe/domain/util/FileService.java
+++ src/main/java/project/studycafe/domain/util/FileService.java
@@ -19,7 +19,7 @@ import project.studycafe.domain.place.model.UpdatePlaceModel;
 public class FileService {
 
 	private final FileMapper fileMapper;
-	private static String IMAGE_PATH = "C:\\image_repo\\";
+	private static String IMAGE_PATH = "/app/was/apache-tomcat-9.0.87/webapps/upload/";
 
 	public void uploadUserImage(UserImageDTO userImageDTO) throws Exception {
 
