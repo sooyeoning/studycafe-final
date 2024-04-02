8a5981759ebb64595363e3497375375b23f69c0a
sooyeoning <susan0915@nate.com>
2024-04-02 17:52:47 +0900

    feat cors

--- src/main/java/project/studycafe/WebMvcConfig.java
+++ src/main/java/project/studycafe/WebMvcConfig.java
@@ -13,15 +13,14 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		registry.addResourceHandler("/mypage/**")
 				//.addResourceLocations("file:///C:/image_repo/");
 				.addResourceLocations("file:///app/was/apache-tomcat-9.0.87/webapps/upload");
-
 	}
 	
 	@Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
                 .allowedOrigins(
-                		"http://localhost:8080"
-                		//"http://3.37.73.88:8080"
+                		"http://localhost:8080",
+                		"http://3.37.73.88:8080"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
         ;
